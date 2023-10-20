57e2a430bb2ecba21bcc4a2436f8f13e6d92af72
toantranduy3008 <39187689+toantranduy3008@users.noreply.github.com>
2023-10-20 17:12:03 +0700

    commit
    Sửa lại bg image

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,17 @@
 import { defineConfig } from 'vite'
+import basicSsl from '@vitejs/plugin-basic-ssl'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(
-    {
-      include: "**/*.tsx/*.jsx",
-    }
-  )],
+  plugins: [
+    react(
+      {
+        include: "**/*.tsx/*.jsx",
+      }
+    ),
+    basicSsl()
+  ],
   base: '/bankdemo',
   server: {
     proxy: {
