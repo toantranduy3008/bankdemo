787df5e0e0b6cd4219e08fa56938d33cfdc03b3b
toantranduy3008 <39187689+toantranduy3008@users.noreply.github.com>
2023-10-24 10:24:24 +0700

    Commit
    Đổi từ menu cha-con sang menu đồng cấp

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,5 +1,5 @@
 import { NavLink } from '@mantine/core';
-import { IconHome2, IconSearch, IconCash } from '@tabler/icons-react';
+import { IconHome2, IconSearch, IconQrcode, IconCurrencyDollar, IconCurrencyDollarSingapore } from '@tabler/icons-react';
 
 import { NavLink as RouterLink, useLocation } from 'react-router-dom';
 
@@ -9,7 +9,11 @@ const NavBar = () => {
         <>
             <NavLink label="Trang chủ" leftSection={<IconHome2 size="1rem" stroke={1.5} />} component={RouterLink} to={"bankdemo/home"} active={pathname === 'bankdemo/home'} />
             <NavLink label="Tìm kiếm giao dịch" leftSection={<IconSearch size="1rem" stroke={1.5} />} component={RouterLink} to={"bankdemo/inquiry"} active={pathname === 'bankdemo/inquiry'} />
-            <NavLink
+
+            <NavLink label="CK qua mã QR" leftSection={<IconQrcode size="1rem" stroke={1.5} />} component={RouterLink} to={"bankdemo/qr-code"} active={pathname === 'bankdemo/qr-code'} />
+            <NavLink label="CK thông thường" leftSection={<IconCurrencyDollar size="1rem" stroke={1.5} />} component={RouterLink} to={"bankdemo/transfer"} active={pathname === 'bankdemo/transfer'} />
+            <NavLink label="CK với giao dịch lớn" leftSection={<IconCurrencyDollarSingapore size="1rem" stroke={1.5} />} component={RouterLink} to={"bankdemo/batch-transfer"} active={pathname === 'bankdemo/batch-transfer'} />
+            {/* <NavLink
                 label="Chuyển khoản"
                 leftSection={<IconCash size="1rem" stroke={1.5} />}
                 childrenOffset={28}
@@ -18,7 +22,7 @@ const NavBar = () => {
                 <NavLink label="CK qua mã QR" component={RouterLink} to={"bankdemo/qr-code"} active={pathname === 'bankdemo/qr-code'} />
                 <NavLink label="CK thông thường" component={RouterLink} to={"bankdemo/transfer"} active={pathname === 'bankdemo/transfer'} />
                 <NavLink label="CK với giao dịch lớn" component={RouterLink} to={"bankdemo/batch-transfer"} active={pathname === 'bankdemo/batch-transfer'} />
-            </NavLink >
+            </NavLink > */}
         </>
     );
 }
